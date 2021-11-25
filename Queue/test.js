003f19b3f92af4ee55ec72989ba955fc0a101ad9
Spirited Hunger <sjkang920921@gmail.com>
2021-11-26 00:48:11 +0900

    more tests

--- Queue/test.js
+++ Queue/test.js
@@ -1,5 +1,13 @@
 const paveBox = require("./paveBox.js");
 
-test("test", () => {
+test("test1", () => {
   expect(paveBox([1])).toBe(1);
+})
+
+test("test2", () => {
+  expect(paveBox([5, 1, 4, 6])).toBe(3);
+})
+
+test("test3", () => {
+  expect(paveBox([95, 90, 99, 99, 80, 99])).toBe(4);
 })
